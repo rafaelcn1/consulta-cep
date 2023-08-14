ed589bdf38fd4d46be86f96d4974122b8144951d
Rafael Cunha <rafaelcn1@hotmail.com>
2023-08-14 10:10:13 -0300

    Componente consulta criado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatButtonModule } from '@angular/material/button';
+import { ConsultaComponent } from './componentes/consulta/consulta.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConsultaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
